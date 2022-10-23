4b486702d02f60823236130c341c7e58bbb60d17
AsakiRain <1287059631@qq.com>
2022-10-23 23:17:10 +0800

    demostrate

--- src/app/views/webauthn/login/index.vue
+++ src/app/views/webauthn/login/index.vue
@@ -4,45 +4,55 @@
 
 <template>
   <div
-    id="register-layer-top"
-    :class="{ 'register-layer-fadeout-top': close, 'register-layer-fadein-top': !close }"
+    id="login-layer-top"
+    :class="{ 'login-layer-fadeout-top': close, 'login-layer-fadein-top': !close }"
   ></div>
   <div
-    id="register-layer-left"
-    :class="{ 'register-layer-fadeout-left': close, 'register-layer-fadein-left': !close }"
+    id="login-layer-left"
+    :class="{ 'login-layer-fadeout-left': close, 'login-layer-fadein-left': !close }"
   ></div>
   <div
-    id="register-layer-right"
-    :class="{ 'register-layer-fadeout-right': close, 'register-layer-fadein-right': !close }"
+    id="login-layer-right"
+    :class="{ 'login-layer-fadeout-right': close, 'login-layer-fadein-right': !close }"
   ></div>
   <div
-    id="register-layer-bottom"
-    :class="{ 'register-layer-fadeout-bottom': close, 'register-layer-fadein-bottom': !close }"
+    id="login-layer-bottom"
+    :class="{ 'login-layer-fadeout-bottom': close, 'login-layer-fadein-bottom': !close }"
   ></div>
 
-  <div id="register-panel-wrapper" :class="{ 'register-panel-wrapper-out': closeWindow }">
-    <Starter
-      v-if="step == 'starter'"
-      :processStep="processStep"
-      :handleReturn="handleReturn"
-      :ctx="ctx"
-      :setContext="setContext"
-      :close="close"
-    />
-    <Processing
-      v-if="step == 'processing'"
-      :processStep="processStep"
-      :ctx="ctx"
-      :setContext="setContext"
-      :close="close"
-    />
-    <Finish
-      v-if="step == 'finish'"
-      :processStep="processStep"
-      :ctx="ctx"
-      :setContext="setContext"
-      :close="close"
-    />
+  <div id="login-panel-wrapper" :class="{ 'login-panel-wrapper-out': closeWindow }">
+    <Panel id="login-panel" body-style="padding: 44px;" v-loading="loading">
+      <template #default>
+        <div id="login-wrapper" class="main-wrapper" v-if="!isFailed">
+          <div class="login-title">{{ $t("webauthn.login.loading") }}</div>
+          <div class="login-info">{{ $t("webauthn.login.loadinginfo") }}</div>
+          <div class="login-progress">
+            <el-progress
+              :show-text="false"
+              :percentage="75"
+              :format="format"
+              :indeterminate="true"
+            />
+          </div>
+        </div>
+        <div id="error-wrapper" class="main-wrapper" v-if="isFailed">
+          <div class="login-title">{{ $t("webauthn.error.title") }}</div>
+          <div class="login-info">{{ $t("webauthn.login.loadinginfo") }}</div>
+          <div class="login-progress">
+            <el-progress
+              :show-text="false"
+              :percentage="75"
+              :format="format"
+              :indeterminate="true"
+              status="exception"
+            />
+          </div>
+        </div>
+        <el-button size="small" type="danger" plain @click="isFailed = !isFailed">
+          这个按钮用来切换isFailed
+        </el-button>
+      </template>
+    </Panel>
   </div>
   <div>
     <el-row :gutter="20">
@@ -70,22 +80,17 @@
 <script>
 import Panel from "@/components/Panel";
 import { sleep } from "@/app/service/common.js";
-import Starter from "./components/Starter.vue";
-import Processing from "./components/Processing.vue";
-import Finish from "./components/Finish.vue";
+
 // eslint-disable-next-line no-unused-vars
 // import router from "../router";
 export default {
-  components: { Panel, Starter, Processing, Finish },
+  components: { Panel },
   data: function () {
     return {
-      step: "processing",
       close: false,
       closeWindow: false,
       loading: false,
-      ctx: {
-        userName: this.$store.state.userInfo.userName
-      }
+      isFailed: false
     };
   },
   methods: {
@@ -94,21 +99,18 @@ export default {
       this.closeWindow = true;
       await sleep(1500);
       this.$router.back();
-    },
-    setContext(ctx) {
-      this.ctx = ctx;
     }
   }
 };
 </script>
 
 <style scoped>
-.register-panel-wrapper-out {
+.login-panel-wrapper-out {
   opacity: 0;
   z-index: 1;
 }
 
-.register-layer-fadeout-top {
+.login-layer-fadeout-top {
   top: 0;
   left: 0;
   right: 0;
@@ -116,11 +118,11 @@ export default {
   opacity: 0.2;
 }
 
-.register-layer-fadein-top {
+.login-layer-fadein-top {
   bottom: 50vh;
 }
 
-.register-layer-fadeout-bottom {
+.login-layer-fadeout-bottom {
   top: 100vh;
   left: 0;
   right: 0;
@@ -128,11 +130,11 @@ export default {
   opacity: 0.2;
 }
 
-.register-layer-fadein-bottom {
+.login-layer-fadein-bottom {
   top: 50vh;
 }
 
-.register-layer-fadeout-left {
+.login-layer-fadeout-left {
   top: 0;
   left: 100vw;
   right: 0;
@@ -140,11 +142,11 @@ export default {
   opacity: 0.2;
 }
 
-.register-layer-fadein-left {
+.login-layer-fadein-left {
   left: 50vw;
 }
 
-.register-layer-fadeout-right {
+.login-layer-fadeout-right {
   top: 0;
   right: 100vw;
   left: 0;
@@ -152,77 +154,99 @@ export default {
   opacity: 0.2;
 }
 
-.register-layer-fadein-right {
+.login-layer-fadein-right {
   right: 50vw;
 }
 
-#register-layer-top,
-#register-layer-bottom {
+#login-layer-top,
+#login-layer-bottom {
   transition-property: all;
   transition-duration: 1.6s;
   transition-timing-function: cubic-bezier(0.17, 0.99, 0.63, 0.6);
   transition-delay: 0s;
 }
 
-#register-layer-right,
-#register-layer-left {
+#login-layer-right,
+#login-layer-left {
   transition-property: all;
   transition-duration: 0.5s;
   transition-timing-function: cubic-bezier(0.17, 0.99, 0.63, 0.6);
   transition-delay: 0s;
 }
 
-#register-layer-top,
-#register-layer-left,
-#register-layer-right,
-#register-layer-bottom {
+#login-layer-top,
+#login-layer-left,
+#login-layer-right,
+#login-layer-bottom {
   z-index: 998;
   background-color: rgb(228, 228, 228);
   position: fixed;
 }
 
-#register-layer-top {
+#login-layer-top {
   top: 0;
   left: 0;
   right: 0;
 }
 
-#register-layer-bottom {
+#login-layer-bottom {
   bottom: 0;
   left: 0;
   right: 0;
 }
 
-#register-layer-left {
+#login-layer-left {
   top: 0;
   right: 0;
   bottom: 0;
 }
 
-#register-layer-right {
+#login-layer-right {
   top: 0;
   left: 0;
   bottom: 0;
 }
 
-#register-panel-wrapper {
+#login-panel-wrapper {
   position: fixed;
   z-index: 999;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-
   display: flex;
   align-items: center;
-
   transition-property: all;
   transition-duration: 1.5s;
   transition-timing-function: cubic-bezier(1, 0.05, 0.84, 0.74);
   transition-delay: 0s;
 }
 
-.light #register-panel-wrapper {
+.light #login-panel-wrapper {
   background: #e4e4e4;
+}
+
+#login-panel {
+  min-height: 230px;
+  width: 450px;
+  transition: all 0.4s 0.2s;
+}
+
+.main-wrapper {
+  font-size: 16px;
+}
+
+.main-wrapper > * {
+  margin-bottom: 24px;
+}
+
+.login-title {
+  font-size: 24px;
+  font-weight: 600;
+}
+
+.login-progress {
+  margin-top: 50px;
+  width: 100%;
 }
 </style>
